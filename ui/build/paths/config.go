38dc5cb6064d790a77daf9a15f5730fa473c429f
dopaemon <polarisdp@gmail.com>
2022-09-25 17:50:29 +0000

    Revert "ui: build: paths: Allow cpio & nproc."
    This reverts commit 93722f694dad89171b33b1aaf9b2f9757d2270ec.

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -102,10 +102,6 @@ var Configuration = map[string]PathConfig{
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
 
-	// CAF
-	"cpio":    Allowed,
-	"nproc":   Allowed,
-
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
